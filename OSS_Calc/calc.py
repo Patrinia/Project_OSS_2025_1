07486f015d34fe488671c04f29a9db3de544b398
Patrinia <Patriniafolia@naver.com>
2025-06-10 21:20:46 +0900

    [기능추가] 현재 출력되고 있는 값을 2,8,16,10진수로 변환하는 기능 추가. 2,8,16진수일 때 계산하면 10진수로 변환된 값을 출력한다. ex)0b1111 + 1 = 16

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -8,6 +8,7 @@ class Calculator:
         self.root.geometry("300x400")
 
         self.expression = ""
+        self.current_base = 10  # 현재 화면에 출력되고 있는 숫자의 진수를 저장하는 변수
 
         # 입력창
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
@@ -19,7 +20,8 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['=',],
+            ['Bin', 'Oct', 'Hex', 'Dec']
         ]
 
         for row in buttons:
@@ -37,16 +39,47 @@ class Calculator:
     def on_click(self, char):
         if char == 'C':
             self.expression = ""
+            self.current_base = 10
         elif char == '=':
             try:
                 self.expression = str(eval(self.expression))
+                self.current_base = 10
+            except Exception:
+                self.expression = "에러"
+        elif char in ['Bin', 'Oct', 'Hex', 'Dec']:
+            try:
+                # 현재 숫자를 10진수로 변환
+                if self.current_base == 2:  # 현재 2진수
+                    current_num = int(self.expression[2:], 2)
+                elif self.current_base == 8:  # 현재 8진수
+                    current_num = int(self.expression[2:], 8)
+                elif self.current_base == 16:  # 현재 16진수
+                    current_num = int(self.expression[2:], 16)
+                else:  # 현재 10진수
+                    current_num = int(float(self.expression))
+                
+                # 진수 변환 (접두어도 함꼐 출력하여 사용자가 현재 진수를 파악하기 용이하기 한다.)
+                if char == 'Bin':
+                    self.expression = bin(current_num)  # '0b' 2진수
+                    self.current_base = 2
+                elif char == 'Oct':
+                    self.expression = oct(current_num)  # '0o' 8진수
+                    self.current_base = 8
+                elif char == 'Hex':
+                    self.expression = hex(current_num).upper()  # '0x' 16진수
+                    self.current_base = 16
+                elif char == 'Dec': # 10진수는 접두어 없이 출력
+                    self.expression = str(current_num)
+                    self.current_base = 10
             except Exception:
                 self.expression = "에러"
         else:
             self.expression += str(char)
+            if self.current_base != 10:  # 숫자나 연산자 입력 시 10진수로 변환하여 계산시킨다
+                self.current_base = 10
 
         self.entry.delete(0, tk.END)
-        self.entry.insert(tk.END, self.expression)
+        self.entry.insert(0, self.expression)
 
 
 
